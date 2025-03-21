34e70bc6107e2352c073d0ca16aa71279cd4f183
Luis Arias <luis@refugies.info>
2025-03-21 09:21:12 +0100

    chore(client): :construction_worker: Add equivalent rule for turbopack

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -9,6 +9,24 @@ const nextConfig: NextConfig = {
     });
     return config;
   },
+  experimental: {
+    // Keep any webpack config synced with this turbo config to avoid issues
+    // See https://nextjs.org/docs/app/api-reference/config/next-config-js/turbo
+    turbo: {
+      rules: {
+        "*.woff2": {
+          loaders: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[path][name].[hash].[ext]",
+              },
+            },
+          ],
+        },
+      },
+    },
+  },
 };
 
 export default nextConfig;
