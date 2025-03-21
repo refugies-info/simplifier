17fe8045c9da4c94baa904ca05b865c00ebc4320
Luis Arias <luis@refugies.info>
2025-03-21 16:13:22 +0100

    feat(front): :construction: Implemented auth route redirect logic

--- apps/front/src/app/api/auth/route.ts
+++ apps/front/src/app/api/auth/route.ts
@@ -1,3 +1,31 @@
+import { config } from "dotenv";
+
+config();
+
 export const GET = async (req: Request) => {
-  return Response.json({ message: "Hello World" });
+  const requiredEnvVars = [
+    "PROCONNECT_DOMAIN",
+    "PROCONNECT_CLIENT_ID",
+    "PROCONNECT_REDIRECT_URI",
+  ];
+
+  const missingVars = requiredEnvVars.filter((key) => !process.env[key]);
+
+  if (missingVars.length > 0) {
+    return new Response(
+      `Missing required environment variables: ${missingVars.join(", ")}`,
+      { status: 500 }
+    );
+  }
+
+  const url = new URL(`${process.env.PROCONNECT_DOMAIN}/oauth/authorize`);
+  const params = new URLSearchParams({
+    client_id: process.env.PROCONNECT_CLIENT_ID as string,
+    response_type: "code",
+    redirect_uri: process.env.PROCONNECT_REDIRECT_URI as string,
+    scope: "openid email profile phone organizations",
+  });
+
+  url.search = params.toString();
+  return Response.redirect(url.toString());
 };
