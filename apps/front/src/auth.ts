e997bd3034496798493b6d54f514490193d9985a
Luis Arias <luis@refugies.info>
2025-03-28 16:07:22 +0100

    feat(front): :lock: Logged in users are authenticated, otherwise redirect to login page

--- apps/front/src/auth.ts
+++ apps/front/src/auth.ts
@@ -1,6 +1,12 @@
 import NextAuth from "next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth;
+    },
+  },
   providers: [
     {
       id: "proconnect",
@@ -11,8 +17,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       checks: ["nonce", "state"],
       authorization: {
         params: {
-          scope:
-            "openid uid given_name email phone siret usual_name",
+          scope: "openid uid given_name email phone siret usual_name",
         },
       },
       clientId: process.env.PROCONNECT_CLIENT_ID,
